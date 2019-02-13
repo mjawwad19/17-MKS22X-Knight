7ab25af79769f1cfbe8433e61407f44a15d71167
Mohammad Jawwad <mjawwad@stuy.edu>
2019-02-12 20:09:57 -0500

    added the exceptions for solve + immediate false cases

--- KnightBoard.java
+++ KnightBoard.java
@@ -27,15 +27,41 @@ public class KnightBoard{
   /*@throws IllegalStateException when the board contains non-zero values.
   @throws IllegalArgumentException when either parameter is negative
    or out of bounds.*/
-  public boolean solve(int startingRow, int startingCol){}
+  public boolean solve(int startingRow, int startingCol){
+    if (startingRow < 0 || startingCol < 0 || startingRow > board.length || startingCol > board[0].length) {
+      throw new IllegalArgumentException();
+    }
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[i].length; j++) {
+        if (board[i][j] != 0) {
+          throw new IllegalStateException();
+        }
+      }
+    }
+    if (startingRow <= startingCol) {
+      if (startingCol%2 == 1 && startingRow%2 == 1 ||
+          startingRow == 1 ||
+          startingRow == 2 ||
+          startingRow == 4 ||
+          startingRow == 3 && (startingCol == 4 ||
+                              startingCol == 6 ||
+                              startingCol == 8)) {
+        return false;
+      }
+    }
+    return true;
+  }
 
   /*@throws IllegalStateException when the board contains non-zero values.
   @throws IllegalArgumentException when either parameter is negative
    or out of bounds.*/
-  public int countSolutions(int startingRow, int startingCol){}
+  //public int countSolutions(int startingRow, int startingCol){}
 
   //Suggestion:
-  private boolean solveH(int row ,int col, int level){}
+  //private boolean solveH(int row ,int col, int level){}
   // level is the # of the knight
 
-}
+  /*public static void main(String[] args) {
+    KnightBoard a = new KnightBoard(5, 6);
+    System.out.println(a);*/
+  }
