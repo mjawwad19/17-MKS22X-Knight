54df73341996bf2a00f1618ce0a8652856bf9356
Mohammad Jawwad <mjawwad@stuy.edu>
2019-02-12 19:59:54 -0500

    added the basics of constructor and toString

--- KnightBoard.java
+++ KnightBoard.java
@@ -4,11 +4,25 @@ public class KnightBoard{
   Initialize the board to the correct size and make them all 0's
   @throws IllegalArgumentException when either parameter is negative.
   */
-  public KnightBoard(int startingRows,int startingCols){}
+  public KnightBoard(int startingRows,int startingCols){
+    if (startingRows < 0 || startingCols < 0) {
+      throw new IllegalArgumentException();
+    }
+    board = new int[startingRows][startingCols];
+  }
 
-
-
-  public String toString(){}
+  public String toString(){
+    String out = "";
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[i].length; j++) {
+        if (board[i][j] == 0) out += "_ ";
+        else if (board[i][j] < 10) out += " " + board[i][j] + " ";
+        else out += board[i][j] + " ";
+      }
+      out += "\n";
+    }
+    return out;
+  }
 
   /*@throws IllegalStateException when the board contains non-zero values.
   @throws IllegalArgumentException when either parameter is negative
