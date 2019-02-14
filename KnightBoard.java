48a35f4895774ca76bd57d6bc940ac5477e32c02
Mohammad Jawwad <mjawwad@stuy.edu>
2019-02-14 14:39:13 -0500

    Made sure countSolutions works

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,6 +1,13 @@
 public class KnightBoard{
   private int[][] board;
-  private int[][] moves = {{1,2}, {-1,2}, {-1,-2}, {1,-2}, {2,1}, {2, -1}, {-2, 1}, {-2, -1}};
+  private int[][] moves = {{1,2},
+                          {-1,2},
+                          {-1,-2},
+                          {1,-2},
+                          {2,1},
+                          {2, -1},
+                          {-2, 1},
+                          {-2, -1}};
   /*
   Initialize the board to the correct size and make them all 0's
   @throws IllegalArgumentException when either parameter is negative.
@@ -53,7 +60,7 @@ public class KnightBoard{
         startingCol < 0 ||
         startingRow >= board.length ||
         startingCol >= board[0].length) {
-      throw new IllegalArgumentException();
+          throw new IllegalArgumentException();
     }
     for (int i = 0; i < board.length; i++) {
       for (int j = 0; j < board[i].length; j++) {
@@ -65,6 +72,7 @@ public class KnightBoard{
     return counter(startingRow, startingCol, 1);
   }
 
+  // tail end recursion for counting solutions
   private int counter(int row, int col, int level) {
     int sum = 0;
     if (level == board.length* board[0].length) return 1;
@@ -97,7 +105,7 @@ public class KnightBoard{
     return out;
   }
 
-  //just for updating correctly
+  //just  for updating correctly (kind of like a remove so to speak from queens)
   private void reset (int level) {
     for (int i = 0; i < board.length; i++) {
       for (int j = 0; j < board[i].length; j++) {
@@ -105,7 +113,7 @@ public class KnightBoard{
       }
     }
   }
-  //clear if can't be added
+  //clear the board
   private void clear(int level) {
     for (int i = 0; i < board.length; i++) {
       for (int j = 0; j < board[i].length; j++) {
@@ -118,8 +126,18 @@ public class KnightBoard{
   public static void main(String[] args) {
     KnightBoard a = new KnightBoard(7, 4);
     //System.out.println(a);
-    //System.out.println(a.solve(0,0));
+    System.out.println(a.solve(0,0));
     System.out.println(a);
-    System.out.println(a.countSolutions(0,0));
+    KnightBoard b = new KnightBoard(5,5);
+    System.out.println(b);
+    //System.out.println(b.solve(0,0));
+    //System.out.println(b);
+    System.out.println(b.countSolutions(0,0)); //304
+    System.out.println(b.countSolutions(0,1)); // 0 no solution
+    System.out.println(b.solve(0,1)); //false
+    System.out.println(b.countSolutions(0,2)); //56
+    System.out.println(b.countSolutions(2,2)); //64
+    System.out.println(b.solve(1,2)); // false
+    System.out.println(b.countSolutions(1,2)); // 0 no solution
   }
 }
