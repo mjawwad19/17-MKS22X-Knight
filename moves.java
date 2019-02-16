3bafb6d3b71bd90684ec5b7ba649feb5228cc63b
Mohammad Jawwad <mjawwad@stuy.edu>
2019-02-16 01:46:34 -0500

    moves can be found and sorted... time to get this loaded up in KnightBoard

--- moves.java
+++ moves.java
@@ -22,8 +22,21 @@ public class moves {
     return out + "]";
   }
 
+  public int find(int rI, int cI) {
+    for (int i = 0; i < 8; i++) {
+      if (mov.get(i).gR() == rI && mov.get(i).gC() == cI) return i;
+    }
+    return -1;
+  }
+
+  public void sortM(tour a) {
+    mov.add(0, mov.remove(find(a.gR(), a.gC())));
+  }
   public static void main(String[] args) {
     moves a = new moves();
+    System.out.println(a);
+    System.out.println(a.find(-2,-1)); // 7
+    a.sortM(new tour(-2,-1));
     System.out.println(a);
   }
 }
