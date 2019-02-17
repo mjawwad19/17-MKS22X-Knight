aa322b3611554c1f2744ecc67b952a2a72982cb4
Mohammad Jawwad <mjawwad@stuy.edu>
2019-02-17 15:08:04 -0500

    round 3: after discussion on the best way to sort through the moves, the idea of having just a list of all possible moves and sorting that instead [in hindsight this should have struck me earlier], we can now go up to 10 x 10 by solve. count on the other hand  takes time to calc all...

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,268 +1,165 @@
-
-public class KnightBoard{
+import java.util.*;
+public class KnightBoard {
   private int[][] board;
-  private int stR, stC;
-  private boolean stat;
-  private tour[] a = {
-    new tour(1,2), new tour(2,1), new tour(-2, -1), new tour(-1,-2),
-    new tour(-1,2), new tour(2, -1), new tour(-2, 1), new tour(1,-2)
-  };
-  /*
-  Initialize the board to the correct size and make them all 0's
-  @throws IllegalArgumentException when either parameter is negative.
-  */
-  public KnightBoard(int startingRows,int startingCols){
-    if (startingRows < 0 || startingCols < 0) {
-      throw new IllegalArgumentException();
-    }
-    stR = startingRows;
-    stC = startingCols;
-    board = new int[stR][stC];
-  }
+  List<tour>[][] pM; //possible Moves
+  int mR, mC; //max Row and Col
+  final static tour[] moves = new tour[] { new tour(-1, -2), new tour(1, -2), new tour(2, -1), new tour(2, 1),
+                                           new tour(1,2), new tour(-1, 2), new tour(-2, 1), new tour (-2, -1) };
 
-  /*displays the board*/
-  public String toString(){
-    String out = "";
-    for (int i = 0; i < stR; i++) {
-      for (int j = 0; j < stC; j++) {
-        if (board[i][j] == 0) out += "__ ";
-        else if (board[i][j] < 10) out += " " + board[i][j] + " ";
-        else out += board[i][j] + " ";
-      }
-      out += "\n";
-    }
-    return out;
+  private boolean runOff(int row, int col) {
+    return !(row < 0 || col < 0 || row >= mR || col >= mC);
   }
-    /*@throws IllegalStateException when the board contains non-zero values.*/
+  private void clear() {
+    for (int i = 0; i < mR; i++) {
+      for (int j = 0; j < mC;j++) {
+        board[i][j] = 0;
+      }
+    }
+  }
+  /*@throws IllegalStateException when the board contains non-zero values.*/
   private void empty() {
-    for (int i = 0; i < stR; i++) {
-      for (int j = 0; j < stC; j++) {
-        if (board[i][j] != 0) {
+    for (int r = 0; r < mR; r++) {
+      for (int c = 0; c < mC; c++) {
+        if (board[r][c] != 0) {
           throw new IllegalStateException();
         }
       }
     }
   }
+  /*displays the board*/
+  public String toString(){
+    String out = "";
+    for (int r = 0; r < mR; r++) {
+      for (int c = 0; c < mC; c++) {
+        if (board[r][c] == 0) out += "__ ";
+        else if (board[r][c] < 10) out += " " + board[r][c] + " ";
+        else out += board[r][c] + " ";
+      }
+      out += "\n";
+    }
+    return out;
+  }
+  /*
+  Initialize the board to the correct size and make them all 0's
+  @throws IllegalArgumentException when either parameter is negative.
+  */
+  public KnightBoard(int startingRows, int startingCols) {
+    if (startingRows < 0 || startingCols < 0) {
+      throw new IllegalArgumentException();
+    }
+    mR = startingRows;
+    mC = startingCols;
+    board = new int[mR][mC];
+    pM = new List[mR][mC];
 
-  /*@throws IllegalArgumentException when either parameter is negative
-   or out of bounds.*/
-  private void runOff(int startingRow, int startingCol) {
-     if (startingRow < 0 ||
-         startingCol < 0 ||
-         startingRow >= stR ||
-         startingCol >= stC) {
-       throw new IllegalArgumentException();
-     }
-   }
+    for (int r = 0; r < mR; r++) {
+      for (int c = 0; c < mC; c++) {
+        board[r][c] = 0;
+        pM[r][c] = new ArrayList<>();
 
-  public boolean solve(int startingRow, int startingCol){
-    runOff(startingRow, startingCol);
+        for (tour a : moves) {
+          //the n starts for new posiiton
+          int nR = r + a.gR();
+          int nC = c + a.gC();
+          //only add to possibleMoves if THEY CAN be placed on baord
+          if ((nR >= 0 && nR < mR) && (nC >= 0 && nC < mC)) {
+            pM[r][c].add(new tour(nR, nC));
+          }
+        }
+      }
+    }
+    prePM();
+
+  }
+  // sorts the possible moves so we get the least amount of possible moves : through suggestions on the best way to handle this: have a list containing the possible moves
+  private void prePM(){
+    for (int r = 0; r < mR; r++) {
+      for (int c = 0; c < mC; c++) {
+        List<tour> moves = pM[r][c];
+        moves.sort(new Comparator<tour>()
+        {
+          @Override
+          public int compare(tour a, tour b) {
+            int aM = pM[a.gR()][a.gC()].size();
+            int bM = pM[b.gR()][b.gC()].size();
+            return aM - bM;
+          }
+        });
+      }
+    }
+  }
+  //same old same old BUT directly clears this time in worst case scenario
+  private boolean solve(int startingRow, int startingCol) {
+    if (!runOff(startingRow, startingCol)) throw new IllegalArgumentException();
     empty();
-    boolean x = solveH(startingRow, startingCol, 1);
-    if (!stat) clear();
-    return x;
+    boolean solved = solveH(startingRow, startingCol, 1);
+    if (!solved) {
+      clear();
+      return false;
+    }
+    return solved;
   }
 
-  public int countSolutions(int startingRow, int startingCol){
-    runOff(startingRow, startingCol);
+  private boolean solveH(int row, int col, int level) {
+    if (board[row][col] != 0) return false;
+    board[row][col] = level;
+    if (level == mR * mC) return true;
+    for (tour s: pM[row][col]) {
+      //just use the premade pM moves to quickly get an answer
+      if (solveH(s.gR(), s.gC(), level + 1)) {
+        return true;
+      }
+    }
+    //we got nothing if we come here
+    board[row][col] = 0;
+    return false;
+  }
+
+  public int countSolutions(int startingRow, int startingCol) {
+    if (!runOff(startingRow, startingCol)) throw new IllegalArgumentException();
     empty();
     return counter(startingRow, startingCol, 1);
   }
 
-  //simplifies solutions --> only if available can we continue
-  private boolean avail(int r, int c, int level) {
-    if(r + 1 < stR && c + 2 < stC) {
-      if (solveH(r + 1, c + 2, level)) return true;
-    }
-    if(r + 2 < stR && c + 1 < stC) {
-      if (solveH(r + 2, c + 1, level)) return true;
-    }
-    if(c - 1 >= 0 && r + 1 < stR) {
-      if (solveH(r + 1, c - 1, level)) return true;
-    }
-    if(c - 2 >= 0 && r + 1 < stR){
-      if (solveH(r + 1, c -2, level)) return true;
-    }
-    if(r - 1 >= 0 && c + 2 < stC){
-      if (solveH(r - 1, c + 2, level)) return true;
-    }
-    if(r - 2 >= 0 && c + 1 < stC) {
-      if (solveH(r - 2, c + 1, level)) return true;
-    }
-    if(r - 1 >= 0 && c - 2 >= 0) {
-      if (solveH(r - 1, c - 2, level)) return true;
-    }
-    if(r - 2 >= 0 && c - 1 >= 0) {
-      if (solveH(r - 2, c - 1, level)) return true;
-    }
-    return false;
-  }
-  /*private tour[] sortMoves (int row, int col) {
-    tour[] temp = a;
-    tour[] ans;
-    if (row == 0) {
-      if (col == 0) {
-        ans = new tour[] {new tour(2, 1), new tour(1,2)};
-      }
-      if (col == stC -1) {
-        ans = new tour[] {new tour(-1, -2), new tour(-2,-1)};
-      }
-      if (col == 1) {
-        ans = new tour[] {new tour(1,2), new tour(2, 1), new tour(2,-1)};
-      }
-      if (col == stC -2) {
-        ans = new tour[] {new tour(2,1), new tour(2, -1), new tour(1, -2)};
-      }
-      else {
-        ans = new tour[] {new tour(2, 1), new tour(2, -1),
-                        new tour(1, -2), new tour(1, 2)};
-      }
-    }
-    if (row == stR -1) {
-      if (col == 0) {
-        ans = new tour[] {new tour(-1, 2), new tour(-2, 1)};
-      }
-      if (col == stC -1) {
-        ans = new tour[] {new tour(-2, -1), new tour(-1, -2)};
-      }
-      if (col == 1) {
-        ans = new tour[] {new tour(-1, 2), new tour(-2, 1), new tour(-2, -1)};
-      }
-      if (col == stC -2) {
-        ans = new tour[] {new tour(-2, -1), new tour(-2, 1), new tour(-1, -2)};
-      }
-      else {
-        ans = new tour[] {new tour(-1, -2), new tour(-1, 2),
-                          new tour(-2, 1), new tour(-2, -1)};
-
-      }
-    }
-    if (row == 1) {
-      if (col == 1) {
-        ans = new tour[] {new tour(-1, 2), new tour(1,2), new tour(2, 1), new tour(2, -1)};
-      }
-      if (col == stC -2) {
-        ans = new tour[] {new tour(1, -2), new tour(-1, -2), new tour(2, -1), new tour(2, 1)};
-      }
-    }
-    if (row == stR -2) {
-      if (col == 1) {
-      ans = new tour[] {new tour(-1, 2), new tour(1,2), new tour(-2, 1), new tour(-2, -1)};
-      }
-      if (col == stC -2) {
-        ans = new tour[] {new tour(-1, -2), new tour(1, -2), new tour(-2, -1), new tour(-2, 1)};
-      }
-    }
-    if (col == 0) {
-      if (row == 1) {
-        ans = new tour[] {new tour(-1, 2), new tour(1, 2), new tour(2, 1)};
-      }
-      if (row == stR -2) {
-        ans = new tour[] {new tour(-1, 2), new tour(1,2), new tour(-2, 1)};
-      }
-    }
-    if (col == stC -1) {
-      if (row == 1) {
-        ans = new tour[] {new tour(-1, -2), new tour(1, -2), new tour(-2, -1)};
-      }
-      if (row == stC -2) {
-        ans = new tour[] {new tour(-1, -2), new tour(1,-2), new tour(2, -1)};
-      }
-    }
-    if (col == 0 && row != 0 && row != stR -1
-    && row != 1 && row != stR -2) {
-      ans = new tour[] {new tour(-2, 1), new tour(2, 1),
-                        new tour(-1, 2), new tour(1, 2)};
-    }
-    if (col == stC-1 && row != 0 && row != stR -1 &&
-        row != 1 && row != stR -2) {
-      ans = new tour[] {new tour(-2, -1), new tour(2, -1),
-                        new tour(-1, -2), new tour(1, -2)};
-    }
-    else ans = temp;
-    return ans;
-  }
-*/
-  // tail end recursion for counting solutions
   private int counter(int row, int col, int level) {
     int sum = 0;
-    if (level == stR* stC) return 1;
-    //tour[] s = sortMoves(row,col);
-    for (int i = 0; i < a.length; i++) {
+    if (level == mR * mC) return 1;
+    for (int i = 0; i < moves.length; i++) {
       try {
-        if (board[row + a[i].gR()][col + a[i].gC()] == 0) {
+        if (board[row + moves[i].gR()][ col + moves[i].gC()] == 0) {
           board[row][col] = level;
-          sum += counter(row + a[i].gR(), col +a[i].gC(), level + 1);
+          sum += counter(row + moves[i].gR(), col + moves[i].gC(), level + 1);
           board[row][col] = 0;
         }
       }catch (Exception e) {}
-      }
-      return sum;
-  }
-  //tail end recursion for touring through
-  private boolean solveH(int row ,int col, int level) {
-    if (row < 0 || col < 0 || row >= stR || col >= stC) return false;
-    if (level > stR * stC) {
-      stat = true;
-      return true;
     }
-    if (board[row][col] == 0){
-      board[row][col] = level;
-      /*tour[] s = sortMoves(row,col);
-      for (int i = 0; i < s.length; i++) {
-        if (solveH(row + s[i].gR(), col + s[i].gC(), level + 1)) return true;*/
-      level += 1;
-      if(avail(row, col, level)) return true;
-      else board[row][col] = 0;
-      }
-    return false;
-  }
-
-  //just  for updating correctly (kind of like a remove so to speak from queens)
-  /*private void reset (int level) {
-    for (int i = 0; i < stR; i++) {
-      for (int j = 0; j < stC; j++) {
-        if (board[i][j] > level) board[i][j] = 0;
-      }
-    }
-  }*/
-
-  private void clear() {
-    for (int i = 0; i < stR; i++) {
-      for (int j = 0; j < stC;j++) {
-        board[i][j] = 0;
-      }
-    }
+    return sum;
   }
 
   public static void main(String[] args) {
-    /*KnightBoard a = new KnightBoard(7, 4);
-    //System.out.println(a);
-    System.out.println(a.solve(0,0));
-    System.out.println(a);
-    KnightBoard b = new KnightBoard(5,5);
-    System.out.println(b);
-    //System.out.println(b.solve(0,0));
-    //System.out.println(b);
-    System.out.println(b.countSolutions(0,0)); //304
-    System.out.println(b.countSolutions(0,1)); // 0 no solution
-    System.out.println(b.solve(0,1)); //false
-    System.out.println(b.countSolutions(0,2)); //56
-    System.out.println(b.countSolutions(2,2)); //64
-    System.out.println(b.solve(1,2)); // false
-    System.out.println(b.countSolutions(1,2)); // 0 no solution
-    KnightBoard c = new KnightBoard(6,6);
-    System.out.println(c.solve(0,0));
-    System.out.println(c);
-    //System.out.println(c.countSolutions(0,0));*/
-    KnightBoard d = new KnightBoard(7,7);
-    System.out.println(d.solve(0,0));
-    System.out.println(d);
-    //System.out.println(d.countSolutions(0,0));
-    KnightBoard e = new KnightBoard(8,8);
-    System.out.println(e.solve(0,0));
-    System.out.println(e);
-    //System.out.println(d.countSolutions(0,0));*/
-
+    KnightBoard x = new KnightBoard(5,5);
+      System.out.println(x.countSolutions(0,0)); //304
+      System.out.println(x.countSolutions(0,1)); // 0 no solution
+      System.out.println(x.solve(0,1)); //false
+      System.out.println(x.countSolutions(0,2)); //56
+      System.out.println(x.countSolutions(2,2)); //64
+      System.out.println(x.solve(1,2)); // false
+      System.out.println(x.countSolutions(1,2)); // 0 no solution
+    KnightBoard a = new KnightBoard(6,6);
+      a.solve(0,0);
+      System.out.println(a);
+    KnightBoard b = new KnightBoard(7,7);
+      b.solve(0,0);
+      System.out.println(b);
+    KnightBoard c = new KnightBoard(8,8);
+      c.solve(0,0);
+      System.out.println(c);
+    KnightBoard d = new KnightBoard(9,9);
+      d.solve(0,0);
+      System.out.println(d);
+    KnightBoard e = new KnightBoard(10,10);
+      e.solve(0,0);
+      System.out.println(e);
+    //Halle-friggin-luyah
   }
 }
